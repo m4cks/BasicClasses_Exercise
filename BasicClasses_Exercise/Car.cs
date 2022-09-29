7c2710e894b97a195284969814346510b7decefc
Max Schlapfer <max.schlapfer@gmail.com>
2022-09-28 22:45:15 -0500

    first commit

---
+++ BasicClasses_Exercise/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClasses_Exercise
+{
+    internal class Car
+    {
+        public string Make;
+        public string Model;
+        public int Year;  
+    }
+}
---
+++ BasicClasses_Exercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicClasses_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car() { Make = "Honda", Model = "Civic", Year = 2010 };
+            Console.WriteLine($"{myCar.Make} is the make.\n{myCar.Model} is the model.\nBuilt in the year {myCar.Year}."); ;
+        }
+    }
+}
